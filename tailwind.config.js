a2fd2cc1ae36568af1d685a2362f5539e164d5bd
Demonage <tortoisegames431@gmail.com>
2024-02-21 21:46:11 +0300

    Add header with navigation, custom font (Press Start 2p) and background with Arthorias

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,15 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-    content: ["*.{html,js}"],
+    content: ["*.{html,js}", "html/**/*.html"],
     theme: {
-      extend: {},
+      extend: {
+        backgroundImage: {
+          'artorias-image': "url(../img/artorias.jpg)"
+        },
+        fontFamily: {
+          'navigation': ['"Press Start 2P"', "system-ui"],
+        },
+      },
     },
     plugins: [],
   }
